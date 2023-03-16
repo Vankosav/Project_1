7f7231c2f4a4ed2d20f159c0420396948328fcdf
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-03-16 19:19:24 +0100

    remove shapes when game is over

--- Classes/shapes.js
+++ Classes/shapes.js
@@ -17,48 +17,47 @@
     this.img3.src = "/Images/triangle.png"
     }*/
 
-    class Circles {
-        constructor(ctx, canvas) {
-          this.x = 0;
-          this.y = Math.random() * canvas.height;
-          this.width = 30;
-          this.height = 30;
-          this.ctx = ctx;
-          this.type = 'circle';
-          this.img = new Image();
-          this.img.src = "/Images/circle.png";
-        }
-        draw() {
-          this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        }
-        move() {
-          this.x += 3;
-        }
-      }
-      
-      class Squares extends Circles {
-        constructor(ctx, canvas) {
-          super(ctx, canvas);
-          this.type = 'square';
-          this.img = new Image();
-          this.img.src = "/Images/square.png";
-        }
-        move() {
-            this.x += 4;
-          }
-      }
-      
-      class Triangles extends Circles {
-        constructor(ctx, canvas) {
-          super(ctx, canvas);
-          this.type = 'triangle';
-          this.img = new Image();
-          this.img.src = "/Images/triangle.png";
-        }
-        move() {
-            this.x += 2;
-          }
-      }
-      
-      export { Circles, Squares, Triangles };
-      
+class Circles {
+  constructor(ctx, canvas) {
+    this.x = 0;
+    this.y = Math.random() * canvas.height;
+    this.width = 30;
+    this.height = 30;
+    this.ctx = ctx;
+    this.type = "circle";
+    this.img = new Image();
+    this.img.src = "/Images/circle.png";
+  }
+  draw() {
+    this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+  move() {
+    this.x += 3;
+  }
+}
+
+class Squares extends Circles {
+  constructor(ctx, canvas) {
+    super(ctx, canvas);
+    this.type = "square";
+    this.img = new Image();
+    this.img.src = "/Images/square.png";
+  }
+  move() {
+    this.x += 4;
+  }
+}
+
+class Triangles extends Circles {
+  constructor(ctx, canvas) {
+    super(ctx, canvas);
+    this.type = "triangle";
+    this.img = new Image();
+    this.img.src = "/Images/triangle.png";
+  }
+  move() {
+    this.x += 2;
+  }
+}
+
+export { Circles, Squares, Triangles };
--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-import Road from "./Classes/road.js"
-import Flyer from "./Classes/student.js"
+import Road from "./Classes/road.js";
+import Flyer from "./Classes/student.js";
 import { Circles, Squares, Triangles } from "./Classes/shapes.js";
 
 window.onload = () => {
-  document.getElementById('start').onclick = () => {
-    console.log('start button is clicked');
-    if(!gameStart){
+  document.getElementById("start").onclick = () => {
+    if (!gameStart) {
       startGame();
       gameStart = true;
-      
     }
   };
 
@@ -16,30 +14,28 @@ window.onload = () => {
   const ctx = canvas.getContext("2d");
 
   const scoreElement = document.getElementById("score");
-  
 
-  const myStudent = new Flyer (ctx);
-  const circle = [];
-  const square = [];
-  const triangle = [];
+  const myStudent = new Flyer(ctx);
+  let circle = [];
+  let square = [];
+  let triangle = [];
 
   const road = new Road(ctx, canvas);
 
   let speed = 1;
-  let counter = 0; 
+  let counter = 0;
   let gameStart = false;
-  let score = 0; 
+  let score = 0;
   let animationFrame;
   let isButtonPressed = false;
-  
+ 
 
   const speechButton = document.getElementById("speech-button");
   const textSpeech = document.getElementById("instructions");
   const textSpeechSay = textSpeech.innerText;
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = textSpeechSay;
-  
-  
+
   function speakInstructions() {
     if (isButtonPressed) {
       const utterance = new SpeechSynthesisUtterance(textSpeechSay);
@@ -47,15 +43,15 @@ window.onload = () => {
     }
   }
 
-speechButton.addEventListener("click", function() {
-  isButtonPressed = true;
-  speakInstructions();
-});
+  speechButton.addEventListener("click", function () {
+    isButtonPressed = true;
+    speakInstructions();
+  });
 
-let myAudio = new Audio ('./Music/backgroundmusic.mp3');
-let sEffect = new Audio ('/Music/soundeffect.wav');
+  let myAudio = new Audio("./Music/backgroundmusic.mp3");
+  let sEffect = new Audio("/Music/soundeffect.wav");
 
-  function startGame(){
+  function startGame() {
     play();
     moveStudent();
     myAudio.play();
@@ -68,13 +64,13 @@ let sEffect = new Audio ('/Music/soundeffect.wav');
     road.Update(speed);
     myStudent.draw();
 
-    if (counter % 90 === 0)  {
+    if (counter % 90 === 0) {
       circle.push(new Circles(ctx, canvas));
     }
-    if (counter % 95 === 0)  {
+    if (counter % 95 === 0) {
       square.push(new Squares(ctx, canvas));
     }
-    if (counter % 100 === 0)  {
+    if (counter % 100 === 0) {
       triangle.push(new Triangles(ctx, canvas));
     }
 
@@ -82,14 +78,16 @@ let sEffect = new Audio ('/Music/soundeffect.wav');
       shape.draw();
       shape.move();
 
-      if (shape.type === 'circle' &&
-          shape.x > myStudent.x &&
-          shape.x < myStudent.x + myStudent.width &&
-          shape.y > myStudent.y &&
-          shape.y < myStudent.y + myStudent.height) {
+      if (
+        shape.type === "circle" &&
+        shape.x > myStudent.x &&
+        shape.x < myStudent.x + myStudent.width &&
+        shape.y > myStudent.y &&
+        shape.y < myStudent.y + myStudent.height
+      ) {
         score++;
         sEffect.play();
-        circle.splice(index, 1); // remove the collected circle from the shapes array
+        circle.splice(index, 1); 
         scoreElement.textContent = `Score: ${score}`;
       }
     });
@@ -98,11 +96,13 @@ let sEffect = new Audio ('/Music/soundeffect.wav');
       shape.draw();
       shape.move();
 
-      if (shape.type === 'square' &&
-          shape.x > myStudent.x &&
-          shape.x < myStudent.x + myStudent.width &&
-          shape.y > myStudent.y &&
-          shape.y < myStudent.y + myStudent.height) {
+      if (
+        shape.type === "square" &&
+        shape.x > myStudent.x &&
+        shape.x < myStudent.x + myStudent.width &&
+        shape.y > myStudent.y &&
+        shape.y < myStudent.y + myStudent.height
+      ) {
         gameOver();
       }
     });
@@ -111,19 +111,21 @@ let sEffect = new Audio ('/Music/soundeffect.wav');
       shape.draw();
       shape.move();
 
-      if (shape.type === 'triangle' &&
-          shape.x > myStudent.x &&
-          shape.x < myStudent.x + myStudent.width &&
-          shape.y > myStudent.y &&
-          shape.y < myStudent.y + myStudent.height) {
+      if (
+        shape.type === "triangle" &&
+        shape.x > myStudent.x &&
+        shape.x < myStudent.x + myStudent.width &&
+        shape.y > myStudent.y &&
+        shape.y < myStudent.y + myStudent.height
+      ) {
         gameOver();
       }
     });
 
     counter++;
   }
-function moveStudent () {
-  document.addEventListener("keydown", (event) => {
+  function moveStudent() {
+    document.addEventListener("keydown", (event) => {
       switch (event.code) {
         case "ArrowLeft":
           myStudent.moveLeft();
@@ -136,23 +138,19 @@ function moveStudent () {
           break;
         case "ArrowDown":
           myStudent.moveDown();
-          break;  
+          break;
       }
     });
-
-  
   }
 
   function gameOver() {
     cancelAnimationFrame(animationFrame);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'orange';
+    ctx.fillStyle = "orange";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgb(96, 96, 228)';
+    ctx.fillStyle = "rgb(96, 96, 228)";
     ctx.font = "80px Arial";
     ctx.fillText("Game Over", 280, 330);
-    onpause();
+    myAudio.pause();
   }
-  
-  }
-
+};
